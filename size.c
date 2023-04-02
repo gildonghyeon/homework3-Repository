f567eea21d3ea57406ed19f660b97aea7b638347
gildonghyeon <118823021+gildonghyeon@users.noreply.github.com>
2023-04-03 01:39:00 +0900

    Update size.c

--- size.c
+++ size.c
@@ -1,8 +1,9 @@
-//----- 길동현 2022041025 -----//
+
 #include<stdio.h>
 #include<stdlib.h>
 void main()
 {
+printf("----길동현 2022041025----\n");
 int **x;
 printf("sizeof(x) = %lu\n", sizeof(x)); //x의 크기 출력
 printf("sizeof(*x) = %lu\n", sizeof(*x)); //x가 가리키는 값의 크기 출력
